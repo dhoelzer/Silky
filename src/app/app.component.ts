0a9692a02c2c9cb7772d0f386922397be47934ae
David Hoelzer <davidhoelzer@Muninn.local>
2018-01-01 18:47:58 -0500

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Silky';
+  version = '0.1'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SilkQueryService } from './silk-query.service';
+import { TopTalkersComponent } from './displays/topTalkers.component';
+import { StatisticsComponent } from './displays/statistics.component';
+import { TopTCPPortsComponent } from './displays/topTCPPorts.component';
 
+import { LargeTransfer } from './models/largeTransfer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TopTalkersComponent,
+    StatisticsComponent,
+    TopTCPPortsComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [SilkQueryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
